374a5203e14d59b0508ab981409a747851399427
Skai-N <snzeuton30@stuy.edu>
2023-07-30 23:18:55 -0400

    reads credentials from file, then logs in to site

--- script.py
+++ script.py
@@ -5,6 +5,8 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 # from selenium.common import exceptions
 
+import time
+
 options = Options()
 options.add_experimental_option("detach", True)
 
@@ -14,6 +16,19 @@ url = "https://onlinejobs.ph/login/"
 driver.get(url)
 
 search = driver.find_element(By.ID, "login_username")
-print(search)
+
+f = open("credentials", "r")
+
+email = f.readline()
+search.send_keys(email)
+search.send_keys(Keys.RETURN)
+
+search = driver.find_element(By.ID, "login_password")
+
+password = f.readline()
+search.send_keys(password)
+search.send_keys(Keys.RETURN)
+
+time.sleep(5)
 
 driver.quit()
