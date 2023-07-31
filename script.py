668b61aba8ad6febbe865f0aa9cd958742df2347
Skai-N <snzeuton30@stuy.edu>
2023-07-31 12:39:51 -0400

    fixed stale element error

--- script.py
+++ script.py
@@ -3,6 +3,8 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 # from selenium.common import exceptions
 
 import time
@@ -12,23 +14,54 @@ options.add_experimental_option("detach", True)
 
 driver = webdriver.Chrome(options=options)
 
-url = "https://onlinejobs.ph/login/"
+url = "https://onlinejobs.ph/login"
+# url = "https://google.com"
 driver.get(url)
 
-search = driver.find_element(By.ID, "login_username")
+login_username = driver.find_element(By.ID, "login_username")
 
 f = open("credentials", "r")
 
 email = f.readline()
-search.send_keys(email)
-search.send_keys(Keys.RETURN)
+login_username.send_keys(email + Keys.RETURN)
+login_username.send_keys(Keys.RETURN)
 
-search = driver.find_element(By.ID, "login_password")
+time.sleep(.5)
+
+login_password = driver.find_element(By.ID, "login_password")
 
 password = f.readline()
-search.send_keys(password)
-search.send_keys(Keys.RETURN)
+login_password.send_keys(password + Keys.RETURN)
 
-time.sleep(5)
+# time.sleep(2)
 
-driver.quit()
+# print("sleep")
+
+# body = driver.find_element_by_tag_name("body")
+# body.send_keys(Keys.CONTROL + 'r')
+
+# print("bop")
+
+# try:
+#     search = WebDriverWait(driver, 5).until(
+#         EC.presence_of_element_located((By.NAME, "keyword"))
+#     )
+
+#     search = driver.find_element(By.NAME, "keyword")
+#     WebDriverWait(driver, 5).until(EC.staleness_of(search))
+# except :
+#     driver.quit()
+
+# driver.refresh()
+
+# search = driver.find_element(By.ID, "searchform")
+# WebDriverWait(driver, 5).until(EC.staleness_of(search))
+# search = driver.find_element(By.ID, "searchform")
+
+# position = f.readline()
+# search.send_keys(position)
+# search.send_keys(Keys.RETURN)
+
+# time.sleep(5)
+
+# driver.quit()
